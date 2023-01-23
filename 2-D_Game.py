bdd3e8c3bf0779e76266e04fbc92db50d90828c2
rarguet1 <98789086+rarguet1@users.noreply.github.com>
2023-01-23 11:29:03 -0500

    Added ground surface, correct test_surface to sky_surface

--- 2-D_Game.py
+++ 2-D_Game.py
@@ -7,13 +7,15 @@ pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 
 sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    
-    screen.blit(test_surface, (0,0)) # Blit = Block image transfer, esesentially putting on surface on another surface
+
+    screen.blit(ground_surface, (0,0))
+    screen.blit(sky_surface, (0,0)) # Blit = Block image transfer, esesentially putting on surface on another surface
     pygame.display.update()
     clock.tick(60)
