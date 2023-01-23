028f4757dbd2971efc612a88cdb643b589143259
rarguet1 <98789086+rarguet1@users.noreply.github.com>
2023-01-23 13:00:59 -0500

    Drawing with rectangles

--- 2-D_Game.py
+++ 2-D_Game.py
@@ -9,7 +9,9 @@ test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert() # Convert makes images easier to run for python
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-text_surface = test_font.render("My game", False, 'Black') # AA = antialiasing, smoothing text
+
+score_surf = test_font.render("My game", False, (64,64,64)) # AA = antialiasing, smoothing text
+score_rect = score_surf.get_rect(center = (400,50))
 
 snail_surf = pygame.image.load("graphics/snail/snail1.png").convert_alpha() # Same as convert but also removes alpha values, messy backround stuff
 snail_rect = snail_surf.get_rect(bottomright = (600, 300))
@@ -22,17 +24,28 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        # if event.type == pygame.MOUSEMOTION:
+        #     if player_rect.collidepoint(event.pos): print("collision")
 
     screen.blit(sky_surface, (0,0)) # Blit = Block image transfer, esesentially putting on surface on another surface
     screen.blit(ground_surface, (0,300))
-    screen.blit(text_surface, (300,50))
+    pygame.draw.rect(screen, '#c0e8ec', score_rect)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect,10)
+    pygame.draw.ellipse(screen, "Brown",pygame.Rect(50,200,100,100))
+    screen.blit(score_surf, score_rect)
 
     snail_rect.x -= 4
     if snail_rect.right <= 0: # if snail leaves the screen move back to start
-        snail_rect.left(800,300)
+        snail_rect.left = 800
     screen.blit(snail_surf,(snail_rect))
     screen.blit(player_surf, player_rect)
 
+    # if player_rect.colliderect(snail_rect):
+    #     print('collision')
+
+    # mouse_pos = pygame.mouse.get_pos()
+    # if player_rect.collidepoint((mouse_pos)):
+    #     print(pygame.mouse.get_pressed())
 
     pygame.display.update()
     clock.tick(60)
