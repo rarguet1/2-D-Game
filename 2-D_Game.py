85f3569772518efdf53b7d9e04c614542af9372e
rarguet1 <98789086+rarguet1@users.noreply.github.com>
2023-01-23 13:50:26 -0500

    Displaying Score

--- 2-D_Game.py
+++ 2-D_Game.py
@@ -1,17 +1,25 @@
 import pygame
 from sys import exit
 
+def display_score():
+    current_time = int(pygame.time.get_ticks() / 1000 - start_time)
+    score_surf = test_font.render(f'Score: {current_time}', False, (64,64,64))
+    score_rect = score_surf.get_rect(center = (400,50))
+    screen.blit(score_surf, score_rect)
+
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
+game_active = True
+start_time = 0
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert() # Convert makes images easier to run for python
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
-score_surf = test_font.render("My game", False, (64,64,64)) # AA = antialiasing, smoothing text
-score_rect = score_surf.get_rect(center = (400,50))
+# score_surf = test_font.render("My game", False, (64,64,64)) # AA = antialiasing, smoothing text
+# score_rect = score_surf.get_rect(center = (400,50))
 
 snail_surf = pygame.image.load("graphics/snail/snail1.png").convert_alpha() # Same as convert but also removes alpha values, messy backround stuff
 snail_rect = snail_surf.get_rect(bottomright = (600, 300))
@@ -25,33 +33,47 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
-                player_gravity = -20
-       
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
-             player_gravity = -20
-       
-    screen.blit(sky_surface, (0,0)) # Blit = Block image transfer, esesentially putting on surface on another surface
-    screen.blit(ground_surface, (0,300))
-    pygame.draw.rect(screen, '#c0e8ec', score_rect)
-    pygame.draw.rect(screen, '#c0e8ec', score_rect,10)
-    screen.blit(score_surf, score_rect)
 
-    snail_rect.x -= 4
-    if snail_rect.right <= 0: # if snail leaves the screen move back to start
-        snail_rect.left = 800
-    screen.blit(snail_surf,(snail_rect))
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
+                    player_gravity = -20
+        
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                    player_gravity = -20
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                snail_rect.left = 800
+                start_time = int(pygame.time.get_ticks() / 1000)
 
-    # Player
-    player_gravity += 1
-    player_rect.y += player_gravity
-    if player_rect.bottom >= 300:
-        player_rect.bottom = 300
-    screen.blit(player_surf, player_rect)
+    if game_active:
+        screen.blit(sky_surface, (0,0)) # Blit = Block image transfer, esesentially putting on surface on another surface
+        screen.blit(ground_surface, (0,300))
+        # pygame.draw.rect(screen, '#c0e8ec', score_rect)
+        # pygame.draw.rect(screen, '#c0e8ec', score_rect,10)
+        # screen.blit(score_surf, score_rect)
+        display_score()
 
+        snail_rect.x -= 4
+        if snail_rect.right <= 0: # if snail leaves the screen move back to start
+            snail_rect.left = 800
+        screen.blit(snail_surf,(snail_rect))
 
+        # Player
+        player_gravity += 1
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 300:
+            player_rect.bottom = 300
+        screen.blit(player_surf, player_rect)
+
+        # collision
+        if snail_rect.colliderect(player_rect):
+            game_active = False
+    else:
+        screen.fill("yellow")
+        
 
     pygame.display.update()
     clock.tick(60)
