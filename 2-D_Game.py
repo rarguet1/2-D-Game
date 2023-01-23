f444a4311658389eb310d768afd69379d827ce66
rarguet1 <98789086+rarguet1@users.noreply.github.com>
2023-01-23 11:45:46 -0500

    Basic Animations

--- 2-D_Game.py
+++ 2-D_Game.py
@@ -7,9 +7,13 @@ pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
+sky_surface = pygame.image.load('graphics/Sky.png').convert() # Convert makes images easier to run for python
+ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface = test_font.render("My game", False, 'Black') # AA = antialiasing, smoothing text
+
+snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha() # Same as convert but also removes alpha values, messy backround stuff
+snail_x_pos = 600
+snail_y_pos = 300
 
 while True:
     for event in pygame.event.get():
@@ -20,6 +24,10 @@ while True:
     screen.blit(sky_surface, (0,0)) # Blit = Block image transfer, esesentially putting on surface on another surface
     screen.blit(ground_surface, (0,300))
     screen.blit(text_surface, (300,50))
+    snail_x_pos -= 4
+    if snail_x_pos < -100:
+        snail_x_pos = 800
+    screen.blit(snail_surface,(snail_x_pos,250))
 
     pygame.display.update()
     clock.tick(60)
