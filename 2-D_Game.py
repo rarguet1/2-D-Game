1a0493b2d4b0c530b37e4b19f4a7f31971d364e1
rarguet1 <98789086+rarguet1@users.noreply.github.com>
2023-01-23 11:36:10 -0500

    Adding Font

--- 2-D_Game.py
+++ 2-D_Game.py
@@ -5,9 +5,11 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
+test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
+text_surface = test_font.render("My game", False, 'Black') # AA = antialiasing, smoothing text
 
 while True:
     for event in pygame.event.get():
@@ -15,7 +17,9 @@ while True:
             pygame.quit()
             exit()
 
-    screen.blit(ground_surface, (0,0))
     screen.blit(sky_surface, (0,0)) # Blit = Block image transfer, esesentially putting on surface on another surface
+    screen.blit(ground_surface, (0,300))
+    screen.blit(text_surface, (300,50))
+
     pygame.display.update()
     clock.tick(60)
