89e58c838ae77b90819905ab99738cee3476c1ce
rarguet1 <98789086+rarguet1@users.noreply.github.com>
2023-01-23 15:25:48 -0500

    Transforming surfaces

--- 2-D_Game.py
+++ 2-D_Game.py
@@ -6,6 +6,7 @@ def display_score():
     score_surf = test_font.render(f'Score: {current_time}', False, (64,64,64))
     score_rect = score_surf.get_rect(center = (400,50))
     screen.blit(score_surf, score_rect)
+    return current_time
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
@@ -14,6 +15,7 @@ clock = pygame.time.Clock()
 test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 game_active = True
 start_time = 0
+score = 0
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert() # Convert makes images easier to run for python
 ground_surface = pygame.image.load('graphics/ground.png').convert()
@@ -28,6 +30,16 @@ player_surf = pygame.image.load("graphics/player/player_walk_1.png").convert_alp
 player_rect = player_surf.get_rect(midbottom = (80,300))
 player_gravity = 0
 
+# Intro screen 
+player_stand = pygame.image.load("graphics/player/player_stand.png").convert_alpha()
+player_stand = pygame.transform.rotozoom(player_stand,0,2) #Used to scale image
+player_stand_rect = player_stand.get_rect(center = (400,200))
+
+game_name = test_font.render("Pixel Runner", False, (111,196,169))
+game_name_rect = game_name.get_rect(center = (400,80))
+
+game_message = test_font.render("Press space to run", False, (111,196,169))
+game_message_rect = game_message.get_rect(center = (400,320))
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -54,7 +66,7 @@ while True:
         # pygame.draw.rect(screen, '#c0e8ec', score_rect)
         # pygame.draw.rect(screen, '#c0e8ec', score_rect,10)
         # screen.blit(score_surf, score_rect)
-        display_score()
+        score = display_score()
 
         snail_rect.x -= 4
         if snail_rect.right <= 0: # if snail leaves the screen move back to start
@@ -72,7 +84,17 @@ while True:
         if snail_rect.colliderect(player_rect):
             game_active = False
     else:
-        screen.fill("yellow")
+        screen.fill((94,129,162))
+        screen.blit(player_stand, player_stand_rect)
+
+        score_message = test_font.render(f"Your score: {score}", False,(111,196, 169))
+        score_message_rect = score_message.get_rect(center = (400,330))
+        screen.blit(game_name, game_name_rect)
+
+        if score == 0:
+            screen.blit(game_message, game_message_rect)
+        else:
+            screen.blit(score_message,score_message_rect)
         
 
     pygame.display.update()
