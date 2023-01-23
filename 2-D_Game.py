bb7b490de19233a4eafaadc780f077dd23c25ea2
rarguet1 <98789086+rarguet1@users.noreply.github.com>
2023-01-23 13:21:39 -0500

    jumping and gravity

--- 2-D_Game.py
+++ 2-D_Game.py
@@ -18,20 +18,21 @@ snail_rect = snail_surf.get_rect(bottomright = (600, 300))
 
 player_surf = pygame.image.load("graphics/player/player_walk_1.png").convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80,300))
+player_gravity = 0
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        # if event.type == pygame.MOUSEMOTION:
-        #     if player_rect.collidepoint(event.pos): print("collision")
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if player_rect.collidepoint(event.pos):
+                player_gravity = -20
+       
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-             print("jump")
-        if event.type == pygame.KEYUP:
-            print("key up")
-
+             player_gravity = -20
+       
     screen.blit(sky_surface, (0,0)) # Blit = Block image transfer, esesentially putting on surface on another surface
     screen.blit(ground_surface, (0,300))
     pygame.draw.rect(screen, '#c0e8ec', score_rect)
@@ -43,18 +44,13 @@ while True:
     if snail_rect.right <= 0: # if snail leaves the screen move back to start
         snail_rect.left = 800
     screen.blit(snail_surf,(snail_rect))
+
+    # Player
+    player_gravity += 1
+    player_rect.y += player_gravity
     screen.blit(player_surf, player_rect)
 
-    # keys = pygame.key.get_pressed()
-    # if keys[pygame.K_SPACE]:
-    #     print("jump")
 
-    # if player_rect.colliderect(snail_rect):
-    #     print('collision')
-
-    # mouse_pos = pygame.mouse.get_pos()
-    # if player_rect.collidepoint((mouse_pos)):
-    #     print(pygame.mouse.get_pressed())
 
     pygame.display.update()
     clock.tick(60)
